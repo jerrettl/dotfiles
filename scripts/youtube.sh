96641637df3d44a27f917aa84dd4e3e44c99bcc8
Jerrett <jerrett7@gmail.com>
2017-06-15 21:46:16 -0400

    Begin yt script conditions

--- scripts/youtube.sh
+++ scripts/youtube.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
 
-echo 'Viewing '$1' with format '$2
-mpv $(youtube-dl -f $2 -g $1)
+if [ -n $2 ]
+then
+	echo 'Viewing '$1' with format '$2
+	mpv $(youtube-dl -f $2 -g $1)
+fi
+
+if [ -z $2 ]
+then
+	echo 'Viewing '$1' with format 22'
+	mpv $(youtube-dl -f 22 -g $1)
+fi
