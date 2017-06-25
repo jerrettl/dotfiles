7f1678145285527d80501f4367dd83022069decb
Jerrett Longworth <jerrett7@gmail.com>
2017-06-25 13:42:54 -0400

    Make file transfers verbose

--- install.sh
+++ install.sh
@@ -3,27 +3,27 @@
 curl -s https://gist.githubusercontent.com/Tadly/0e65d30f279a34c33e9b/raw/pacaur_install.sh | bash
 
 pacaur -Syyu
-pacaur -S --needed calc compton git i3-wm i3blocks npm ranger tmux tree vim xorg-xrdb youtube-dl zathura zsh 
+pacaur -S --needed calc compton git i3-wm i3blocks npm ranger tmux tree vim xorg-xrdb youtube-dl zathura zsh
 
-ln -sf ~/dotfiles/.compton.conf ~/.compton.conf
-ln -sf ~/dotfiles/.config/i3/config ~/.config/i3/config
-ln -sf ~/dotfiles/.config/i3blocks/config ~/.config/i3blocks/config
-ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -sf ~/dotfiles/.vim ~/.vim
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.Xresources ~/.Xresources
+ln -sfv ~/dotfiles/.compton.conf ~/.compton.conf
+ln -sfv ~/dotfiles/.config/i3/config ~/.config/i3/config
+ln -sfv ~/dotfiles/.config/i3blocks/config ~/.config/i3blocks/config
+ln -sfv ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sfv ~/dotfiles/.vim ~/.vim
+ln -sfv ~/dotfiles/.vimrc ~/.vimrc
+ln -sfv ~/dotfiles/.Xresources ~/.Xresources
+ln -sfv ~/dotfiles/.zshrc ~/.zshrc
 xrdb ~/.Xresources
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
 
 git config --global user.email "jerrett7@gmail.com"
 git config --global user.name "Jerrett Longworth"
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-pacaur -S --needed awk i3lock-color-git imagemagick 
+pacaur -S --needed awk i3lock-color-git imagemagick
 git clone https://github.com/meskarune/i3lock-fancy
-sudo cp -rf i3lock-fancy/icons /usr/local/bin
-sudo cp -f i3lock-fancy/lock /usr/local/bin
-rm -rf i3lock-fancy
+sudo cp -rfv i3lock-fancy/icons /usr/local/bin
+sudo cp -fv i3lock-fancy/lock /usr/local/bin
+rm -rfv i3lock-fancy
 
 sudo npm install -g castnow npm
