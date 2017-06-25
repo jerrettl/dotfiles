7bfc5adaffe3617c689a5245aa7a41aab88e1c7d
Jerrett Longworth <jerrett7@gmail.com>
2017-06-25 12:54:04 -0400

    Add a few more packages and alphabetize them

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 curl -s https://gist.githubusercontent.com/Tadly/0e65d30f279a34c33e9b/raw/pacaur_install.sh | bash
 
 pacaur -Syyu
-pacaur -S --needed git i3-wm i3blocks vim npm tree xorg-xrdb tmux compton zsh calc
+pacaur -S --needed calc compton git i3-wm i3blocks npm ranger tmux tree vim xorg-xrdb youtube-dl zathura zsh 
 
 ln -sf ~/dotfiles/.compton.conf ~/.compton.conf
 ln -sf ~/dotfiles/.config/i3/config ~/.config/i3/config
@@ -20,8 +20,10 @@ git config --global user.name "Jerrett Longworth"
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-pacaur -S --needed i3lock-color-git imagemagick awk
+pacaur -S --needed awk i3lock-color-git imagemagick 
 git clone https://github.com/meskarune/i3lock-fancy
 sudo cp -rf i3lock-fancy/icons /usr/local/bin
 sudo cp -f i3lock-fancy/lock /usr/local/bin
 rm -rf i3lock-fancy
+
+sudo npm install -g castnow npm
