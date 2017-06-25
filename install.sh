ba965576a8975dbe620f26d1beaf90dd933441db
Jerrett Longworth <jerrett7@gmail.com>
2017-06-25 10:12:21 -0400

    Update package list

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 curl -s https://gist.githubusercontent.com/Tadly/0e65d30f279a34c33e9b/raw/pacaur_install.sh | bash
 
 pacaur -Syyu
-pacaur -S git i3 i3-bars i3lock-color-git vim npm tree xorg-xrdb
+pacaur -S --needed git i3-wm i3blocks vim npm tree xorg-xrdb tmux compton zsh
 
 ln -sf ~/dotfiles/.compton.conf ~/.compton.conf
 ln -sf ~/dotfiles/.config/i3/config ~/.config/i3/config
@@ -20,6 +20,7 @@ git config --global user.name "Jerrett Longworth"
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
+pacaur -S --needed i3lock-color-git imagemagick awk
 git clone https://github.com/meskarune/i3lock-fancy
 sudo cp -rf i3lock-fancy/icons /usr/local/bin
 sudo cp -f i3lock-fancy/lock /usr/local/bin
