bee3a4ba69d608f838d8504b3784530753ec0c20
Jerrett Longworth <jerrett7@gmail.com>
2017-07-16 00:21:43 -0500

    Add volumeicon package

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ sudo pacman -S --needed curl
 curl -s https://gist.githubusercontent.com/Tadly/0e65d30f279a34c33e9b/raw/pacaur_install.sh | bash
 
 pacaur -Syyu
-pacaur -S --needed calc compton git i3-wm i3blocks lightdm lightdm-gtk-greeter npm otf-san-francisco ranger rxvt-unicode tmux tree ttf-droid ttf-ms-fonts ttf-roboto vim xdotool xorg-server xorg-xrdb youtube-dl zathura zathura-pdf-poppler zsh
+pacaur -S --needed calc compton git i3-wm i3blocks lightdm lightdm-gtk-greeter npm otf-san-francisco ranger rxvt-unicode tmux tree ttf-droid ttf-ms-fonts ttf-roboto vim volumeicon xdotool xorg-server xorg-xrdb youtube-dl zathura zathura-pdf-poppler zsh
 
 ln -sfv ~/dotfiles/.compton.conf ~/.compton.conf
 ln -sfv ~/dotfiles/.config/i3/config ~/.config/i3/config
