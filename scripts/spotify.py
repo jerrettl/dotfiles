b01f9a784f3872318ff84c11e2c2e0903bec286b
Jerrett Longworth <jerrett7@gmail.com>
2017-11-26 17:59:03 -0500

    Escape ampersand See https://github.com/vivien/i3blocks/issues/226

--- scripts/spotify.py
+++ scripts/spotify.py
@@ -6,7 +6,9 @@ try:
 	spotify = bus.get_object("org.mpris.MediaPlayer2.spotify", "/org/mpris/MediaPlayer2")
 	spotify_iface = dbus.Interface(spotify, 'org.freedesktop.DBus.Properties')
 	props = spotify_iface.Get('org.mpris.MediaPlayer2.Player', 'Metadata')
-	print("<span background='#005ab3' color='#ffffff'>" + "  ♫  " +  str(props['xesam:artist'][0]) + " - " + str(props['xesam:title']) + "  </span>")
+
+	output = "<span background='#005ab3' color='#ffffff'>" + "  ♫  " +  str(props['xesam:artist'][0]) + " - " + str(props['xesam:title']) + "  </span>"
+	print(output.replace("&", "&amp;"))
 	exit
 except dbus.exceptions.DBusException:
 	exit
