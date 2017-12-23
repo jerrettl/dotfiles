1f2356ad92f022458849291d8c5f3c3da50003e9
Jerrett Longworth <jerrett7@gmail.com>
2017-12-23 11:41:22 -0500

    urxvt: add perl scripts

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ sudo pacman -S --needed curl
 curl -s https://gist.githubusercontent.com/Tadly/0e65d30f279a34c33e9b/raw/pacaur_install.sh | bash
 
 pacaur -Syyu
-pacaur -S --needed bdf-unifont calc caffeine-ng compton feh firefox git gvim i3-gaps i3blocks keepass libotf lightdm lightdm-gtk-greeter npm pango qt5-webengine-widevine qutebrowser ranger rofi rxvt-unicode scrot system-san-francisco-font-git tmux tree ttf-droid ttf-ms-fonts ttf-roboto unzip volumeicon xdotool xorg-server xorg-xrdb youtube-dl zathura zathura-pdf-poppler zip zsh zsh-autosuggestions
+pacaur -S --needed bdf-unifont calc caffeine-ng compton feh firefox git gvim i3-gaps i3blocks keepass libotf lightdm lightdm-gtk-greeter npm pango qt5-webengine-widevine qutebrowser ranger rofi rxvt-unicode scrot system-san-francisco-font-git tmux tree ttf-droid ttf-ms-fonts ttf-roboto unzip urxvt-perls volumeicon xdotool xorg-server xorg-xrdb youtube-dl zathura zathura-pdf-poppler zip zsh zsh-autosuggestions
 
 ln -sfv ~/dotfiles/.compton.conf ~/.compton.conf
 ln -sfv ~/dotfiles/.config/i3/config ~/.config/i3/config
