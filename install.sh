f84805c1eeec33efc393a22f0212689b3b809214
Jerrett Longworth <jerrett7@gmail.com>
2018-01-03 17:35:30 -0500

    install.sh: minimize level of toxicity

--- install.sh
+++ install.sh
@@ -1,27 +1,83 @@
 #!/bin/bash
 
-sudo pacman -S --needed curl
+echo "Installing pacaur requirements and upgrading all packages..."
+sudo pacman -Syyu --needed curl
+if [ "$(pacman -Qi pacaur)" ]; then
+	echo "Pacaur installed, skipping..."
+else
 curl -s https://gist.githubusercontent.com/Tadly/0e65d30f279a34c33e9b/raw/pacaur_install.sh | bash
+fi
 
-pacaur -Syyu
-pacaur -S --needed bdf-unifont calc caffeine-ng compton feh firefox git gvim i3-gaps i3blocks keepass libotf lightdm lightdm-gtk-greeter msmtp neomutt npm numlockx offlineimap pango qt5-webengine-widevine qutebrowser ranger rofi rxvt-unicode-pixbuf scrot system-san-francisco-font-git tmux tree ttf-droid ttf-ms-fonts ttf-roboto udiskie unzip urxvt-perls volumeicon w3m xdotool xorg-server xorg-xrdb youtube-dl zathura zathura-pdf-poppler zip zsh zsh-autosuggestions
 
+echo "Installing user-required packages..."
+pacaur -Syyu --needed \
+	awk \
+	bdf-unifont \
+	calc \
+	caffeine-ng \
+	compton \
+	feh \
+	firefox \
+	git \
+	gvim \
+	i3-gaps \
+	i3blocks \
+	i3lock-color-git \
+	imagemagick \
+	keepass \
+	libotf \
+	lightdm \
+	lightdm-gtk-greeter \
+	msmtp \
+	neomutt \
+	npm \
+	numlockx \
+	offlineimap \
+	pango \
+	qt5-webengine-widevine \
+	qutebrowser \
+	ranger \
+	rofi \
+	rxvt-unicode-pixbuf \
+	scrot \
+	system-san-francisco-font-git \
+	tmux \
+	tree \
+	ttf-droid \
+	ttf-ms-fonts \
+	ttf-roboto \
+	udiskie \
+	unzip \
+	urxvt-perls \
+	volumeicon \
+	w3m \
+	xdotool \
+	xorg-server \
+	xorg-xrdb \
+	youtube-dl \
+	zathura \
+	zathura-pdf-poppler \
+	zip \
+	zsh \
+	zsh-autosuggestions
+
+
+echo "Creating symlinks..."
 ln -sfv ~/dotfiles/.compton.conf ~/.compton.conf
 ln -sfv ~/dotfiles/.config/i3/config ~/.config/i3/config
 ln -sfv ~/dotfiles/.config/i3blocks/config ~/.config/i3blocks/config
 mkdir ~/.config/pianobar
-if [ ! -f ~/.config/pianobar/ctl ]; then
 mkfifo ~/.config/pianobar/ctl
-fi
 touch ~/.config/pianobar/nowplaying
 ln -sfv ~/dotfiles/.config/pianobar/config ~/.config/pianobar/config
+rm -f ~/.config/qutebrowser
 ln -sfv ~/dotfiles/.config/qutebrowser/ ~/.config/qutebrowser
 ln -sfv ~/dotfiles/.config/ranger/rc.conf ~/.config/ranger/rc.conf
 ln -sfv ~/dotfiles/.config/ranger/scope.sh ~/.config/ranger/scope.sh
 ln -sfv ~/dotfiles/.config/rofi/config.rasi ~/.config/rofi/config.rasi
-rm -f ~/msmtp
+rm -f ~/.msmtp
 ln -sfv ~/dotfiles/.msmtp/ ~/.msmtp
-cp -nv ~/dotfiles/.msmtprc-example ~/dotfiles/.msmtprc
+cp -nv ~/dotfiles/.msmtprc-example ~/.msmtprc
 mkdir ~/.neomutt
 ln -sfv ~/dotfiles/.neomutt/colors ~/.neomutt/colors
 ln -sfv ~/dotfiles/.neomutt/macros ~/.neomutt/macros
@@ -31,6 +87,7 @@ cp -nv ~/dotfiles/.neomutt/user1-example ~/.neomutt/user1
 ln -sfv ~/dotfiles/.offlineimap.py ~/.offlineimap.py
 cp -nv ~/dotfiles/.offlineimaprc-example ~/.offlineimaprc
 ln -sfv ~/dotfiles/.tmux.conf ~/.tmux.conf
+rm ~/.vim
 ln -sfv ~/dotfiles/.vim/ ~/.vim
 ln -sfv ~/dotfiles/.vimrc ~/.vimrc
 ln -sfv ~/dotfiles/.Xresources ~/.Xresources
@@ -41,7 +98,8 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+cd ~/.oh-my-zsh/custom/themes/powerlevel9k
+git pull
+cd ~
 
-pacaur -S --needed awk i3lock-color-git imagemagick
-
-sudo npm install -g castnow npm
+sudo npm install -g npm
