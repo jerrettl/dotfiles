7a831da89d6809954c6d3dfb58a9d9b812af6880
Jerrett Longworth <jerrett7@gmail.com>
2018-01-03 14:21:01 -0500

    make the youtube script work

--- scripts/youtube.sh
+++ scripts/youtube.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 
-if [ -n $2 ]
-then
-	echo 'Viewing '$1' with format '$2
-	mpv $(youtube-dl -f $2 -g $1)
+if [ ! -f "$2" ]; then
+	format="22"
+else
+	format="$2"
 fi
 
-if [ -z $2 ]
-then
-	echo 'Viewing '$1' with format 22'
-	mpv $(youtube-dl -f 22 -g $1)
-fi
+echo 'Viewing '$1' with format '$format
+mpv $(youtube-dl -f $format -g $1)
+
