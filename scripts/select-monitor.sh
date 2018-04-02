5a067cb8977dba72d4b6b78071604554ba236d1c
Jerrett Longworth <jerrett7@gmail.com>
2018-04-01 21:18:25 -0400

    select-monitor.sh: add inspiron-15-3537 rules
    This file will *probably* get a better approach later but for now it works

--- scripts/select-monitor.sh
+++ scripts/select-monitor.sh
@@ -11,6 +11,15 @@ if [ "$hostname" == "arch-aspire-d255" ]; then
 	elif [ "$action" == "mirror" ]; then
 		xrandr --output LVDS1 --rate 60 --mode 800x600 --fb 800x600 --panning 800x600 --output VGA1 --mode 800x600 --same-as LVDS1
 	fi
+
+elif [ "$hostname" == "inspiron-15-3537" ]; then
+	if [ "$action" == "primary" ]; then
+		xrandr --output eDP1 --auto --output HDMI1 --off
+	elif [ "$action" == "secondary" ]; then
+		xrandr --output eDP1 --off --output HDMI1 --auto
+	elif [ "$action" == "mirror" ]; then
+		xrandr --output eDP1 --rate 60 --mode 1280x720 --fb 1280x720 --panning 1280x720 --output HDMI1 --mode 1280x720 --same-as eDP1
+	fi
 fi
 
 feh --bg-fill ~/dotfiles/wallpaper.png
