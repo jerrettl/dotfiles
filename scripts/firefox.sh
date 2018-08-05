4f7c392430cdb164e23ecd8fa2e8e51e46b705b6
Jerrett Longworth <jerrett7@gmail.com>
2018-08-05 16:27:59 -0400

    Remove old scripts

--- scripts/firefox.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-firefox -new-tab -url 'https://mail.google.com/mail/u/0/h/1b3yvbfwierm9/?zy=d&f=1' -new-tab -url 'http://voice.google.com' -new-tab -url 'http://web.groupme.com' -new-tab -url 'http://web.telegram.org'
--- scripts/terminal.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-hostname=$(uname -n)
-
-if [ "$hostname" == "arch-aspire-d255" ]; then
-	urxvt --meta8
-else
-	kitty
-fi
