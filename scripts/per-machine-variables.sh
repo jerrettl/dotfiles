d58fb4c9d1a8b3941fe54558633ddc01355ae7b5
Jerrett Longworth <jerrett7@gmail.com>
2018-09-07 21:33:18 -0400

    per-machine-variables.sh: Adjust xinput settings This should be much more versatile for the usual id changes that occur from day to day.

--- scripts/per-machine-variables.sh
+++ scripts/per-machine-variables.sh
@@ -3,5 +3,12 @@
 hostname=$(uname -n)
 
 if [ "$hostname" == "lg-gram" ]; then
+	# Display scaling
 	export GDK_SCALE=2
+
+	# Touchpad acceleration adjustment
+	id=$(xinput list | grep -i "Touchpad" | grep -oP 'id=[0-9]*' | sed 's/id=//')
+	property=$(xinput list-props "$id" | grep -i 'libinput Accel Speed (' | cut -d "(" -f2 | cut -d ")" -f1)
+	accel="0.4"
+	xinput set-prop "$id" "$property" "$accel"
 fi
