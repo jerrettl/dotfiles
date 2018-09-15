bc0eb99cd30c45682275afe426eccfd1c6ed29a7
Jerrett Longworth <jerrett7@gmail.com>
2018-09-15 15:45:32 -0400

    i3: Add automatic pacman update checker and downloader

---
+++ scripts/pacman-i3.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+update="pkill -RTMIN+2 i3blocks"
+
+case "$BLOCK_BUTTON" in
+	1) urxvt -e sudo pacman -Su ;;
+esac
+
+# This command does not need a password since an exception has been created for it in sudoers
+sudo pacman -Sy 2>&1 >/dev/null
+
+# If there are no updates, stop
+number=$(pacman -Qu | wc -l)
+if [ "$number" == "0" ]; then exit; fi
+
+# If there are updates, download them to local storage
+sudo pacman -Sywu --noconfirm 2>&1 >/dev/null
+
+# ad3a00 - orange
+# 007018 - green
+echo "<span color='#ffffff' background='#007018'>  $number Updates  </span>"
+$update
