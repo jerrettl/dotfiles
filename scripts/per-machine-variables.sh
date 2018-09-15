2ab1b1270aa29215c1e68e6ced13d8f598aaba93
Jerrett Longworth <jerrett7@gmail.com>
2018-09-15 15:44:54 -0400

    Define key hold repeat timing

--- scripts/per-machine-variables.sh
+++ scripts/per-machine-variables.sh
@@ -11,4 +11,7 @@ if [ "$hostname" == "lg-gram" ]; then
 	property=$(xinput list-props "$id" | grep -i 'libinput Accel Speed (' | cut -d "(" -f2 | cut -d ")" -f1)
 	accel="0.4"
 	xinput set-prop "$id" "$property" "$accel"
+
+	# Key press repeat timing
+	xset r rate 250 40
 fi
