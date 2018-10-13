b5e2f41b28db2ef019662b5c307ef7c2d02b8907
Jerrett Longworth <jerrett7@gmail.com>
2018-10-13 13:20:46 -0400

    per-machine-variables.sh: Add wireless mouse sensitivity

--- scripts/per-machine-variables.sh
+++ scripts/per-machine-variables.sh
@@ -12,6 +12,12 @@ if [ "$hostname" == "lg-gram" ]; then
 	accel="0.4"
 	xinput set-prop "$id" "$property" "$accel"
 
+	# Wireless mouse sensitivity
+	id=$(xinput list | grep -i "MOSART Semi. 2.4G Wireless Mouse Mouse" | grep -oP 'id=[0-9]*' | sed 's/id=//')
+	property=$(xinput list-props "$id" | grep -i 'libinput Accel Speed (' | cut -d "(" -f2 | cut -d ")" -f1)
+	accel="0.2"
+	xinput set-prop "$id" "$property" "$accel"
+
 	# Key press repeat timing
 	xset r rate 250 40
 
