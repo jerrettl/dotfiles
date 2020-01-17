dbfdeb47f1cb7d6dfeeb374d033826c7e2a9cae8
Jerrett Longworth <jerrett7@gmail.com>
2020-01-17 13:54:17 -0500

    chrome-quickopen.sh: Add dmenu theme configuration

--- scripts/chrome-quickopen.sh
+++ scripts/chrome-quickopen.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-site=$(cat ~/dotfiles/websites.txt | dmenu -p 'Enter a URL:' -i -l 10)
+site=$(cat ~/dotfiles/websites.txt | dmenu -p 'Enter a URL:' -i -l 10 -nb "#303030" -nf "#ffffff" -sb "#ffffff" -sf "#303030" -fn "HelveticaNeue-11")
 if [ "$?" == "0" ]; then chromium $site; fi
