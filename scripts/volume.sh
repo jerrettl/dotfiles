057a629b2288410a78d927eb07cba14d72659e4e
Jerrett Longworth <jerrett7@gmail.com>
2020-01-17 13:52:08 -0500

    volume.sh: Clean up and make more reliable

--- scripts/volume.sh
+++ scripts/volume.sh
@@ -12,18 +12,13 @@ if [ "$1" = "level" ]; then
 	fi
 fi
 
-#xdotool key ctrl+space
-#xdotool key ctrl+space
-
-sink_number=$(pactl list sinks | awk 'NR==1 {print $2}' | cut -c 2-)
 
 if [ "$1" = "up" ]; then
-	pactl -- set-sink-volume "$sink_number" +5%
+	amixer -q sset Master 5%+
 elif [ "$1" = "down" ]; then
-	pactl -- set-sink-volume "$sink_number" -5%
+	amixer -q sset Master 5%-
 elif [ "$1" = "toggle" ]; then
-	pactl set-sink-mute "$sink_number" toggle
+	amixer -q sset Master toggle
 fi
 
 pkill -RTMIN+1 i3blocks
-#notify-send -t 300 -c volume 'Volume' "$(getlevel)"
