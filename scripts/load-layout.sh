062d76be41d1cf2e521de3a061356a0702d5d12e
Jerrett Longworth <jerrett7@gmail.com>
2020-02-28 11:31:49 -0500

    zathura/load-layout.sh: Make separate config files for gui vs. nogui

--- scripts/load-layout.sh
+++ scripts/load-layout.sh
@@ -10,7 +10,7 @@ if [ "$?" == "1" ]; then exit; fi
 
 if [ "$selection" == "World History Outlining" ]; then
   i3-msg "workspace $workspace; append_layout ~/dotfiles/scripts/layouts/woh_layout.json"
-  zathura -c ~/dotfiles/.config/zathura /mnt/shared/School/Textbooks/Robert\ W.\ Strayer\,\ Eric\ W.\ Nelson\ -\ Ways\ of\ the\ World_\ A\ Brief\ Global\ History\ with\ Sources\,\ Combined\ Volume-Bedford_St.\ Martin’s\ \(2015\).pdf &
+  zathura -c ~/dotfiles/.config/zathura/nogui /mnt/shared/School/Textbooks/Robert\ W.\ Strayer\,\ Eric\ W.\ Nelson\ -\ Ways\ of\ the\ World_\ A\ Brief\ Global\ History\ with\ Sources\,\ Combined\ Volume-Bedford_St.\ Martin’s\ \(2015\).pdf &
   #urxvt -e ranger /mnt/shared/School/woh-2012/Notes/ &
   emacs /mnt/shared/School/woh-2012/Notes/ &
 fi
