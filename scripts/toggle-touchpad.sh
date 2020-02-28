45bc40c1039668dd44943a4f1311c6cec15c3c08
Jerrett Longworth <jerrett7@gmail.com>
2020-02-28 11:35:05 -0500

    i3: Add toggle touchpad binding

---
+++ scripts/toggle-touchpad.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+# Grabbed from https://github.com/lahwaacz/Scripts/blob/master/toggle-touchpad.sh
+
+device="04CA00A0:00 04CA:00A0 Touchpad"
+enabled=$(xinput --list-props "$device" | grep "Device Enabled" | awk '{print $NF}')
+
+if [[ "$enabled" == "1" ]]; then
+    xinput --disable "$device"
+    echo 0 | sudo tee /sys/devices/platform/lg-laptop/leds/tpad_led/brightness
+else
+    xinput --enable "$device"
+    echo 1 | sudo tee /sys/devices/platform/lg-laptop/leds/tpad_led/brightness
+fi
