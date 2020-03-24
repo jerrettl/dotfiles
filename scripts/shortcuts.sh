1a5a4fd81cf9c0af0ab60ba3296f1b33d99ebd7d
Jerrett Longworth <jerrett7@gmail.com>
2020-03-24 19:53:10 -0400

    shortcuts.sh: Allow paths to have spaces
    https://stackoverflow.com/questions/17091500/awk-command-to-print-until-end-of-line/31100196#31100196

--- scripts/shortcuts.sh
+++ scripts/shortcuts.sh
@@ -15,14 +15,14 @@ rm -f "$bash_shortcuts" "$ranger_shortcuts" "$qute_shortcuts"
 
 
 # Folder bindings
-sed "/^dotf/d" "$folders" | awk '{print "alias "$1"=\"cd "$2" && l\""}' >> "$bash_shortcuts"
-cat "$folders" | awk '{print "map g"$1" cd "$2"\nmap t"$1" tab_new "$2"\nmap m"$1" shell mv %s "$2"\nmap Y"$1" shell cp -r %s "$2""}' >> "$ranger_shortcuts"
-cat "$folders" | awk '{print "config.bind(\";"$1"\", \"set downloads.location.directory "$2" ;; hint links download\")"}' >> "$qute_shortcuts"
+sed "/^dotf/d" "$folders" | awk '{print "alias "$1"=\"cd "substr($0, index($0, $2))" && l\""}' >> "$bash_shortcuts"
+cat "$folders" | awk '{print "map g"$1" cd "substr($0, index($0, $2))"\nmap t"$1" tab_new "substr($0, index($0, $2))"\nmap m"$1" shell mv %s "substr($0, index($0, $2))"\nmap Y"$1" shell cp -r %s "substr($0, index($0, $2))""}' >> "$ranger_shortcuts"
+cat "$folders" | awk '{print "config.bind(\";"$1"\", \"set downloads.location.directory "substr($0, index($0, $2))" ;; hint links download\")"}' >> "$qute_shortcuts"
 
 
 
 # Config bindings
-cat "$configs" | awk '{print "alias "$1"=\"$EDITOR "$2"\""}' >> "$bash_shortcuts"
-cat "$configs" | awk '{print "map "$1" shell $EDITOR "$2""}' >> "$ranger_shortcuts"
+cat "$configs" | awk '{print "alias "$1"=\"$EDITOR "substr($0, index($0, $2))"\""}' >> "$bash_shortcuts"
+cat "$configs" | awk '{print "map "$1" shell $EDITOR "substr($0, index($0, $2))""}' >> "$ranger_shortcuts"
 
 source ~/.bashrc
