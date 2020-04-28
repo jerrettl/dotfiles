708c7f866d4fb1f288e9918532c92ddafa42aec3
Jerrett Longworth <jerrett7@gmail.com>
2020-04-28 19:15:17 -0400

    layouts: Remove world history outlining
    No longer used

--- scripts/load-layout.sh
+++ scripts/load-layout.sh
@@ -8,12 +8,7 @@ if [ "$?" == "1" ]; then exit; fi
 workspace=$(echo | dmenu -nb "#303030" -nf "#ffffff" -sb "#ffffff" -sf "#303030" -fn "HelveticaNeue-11" -p "What workspace would you like to place it on?")
 if [ "$?" == "1" ]; then exit; fi
 
-if [ "$selection" == "World History Outlining" ]; then
-  i3-msg "workspace $workspace; append_layout ~/dotfiles/scripts/layouts/woh_layout.json"
-  zathura -c ~/dotfiles/.config/zathura/nogui /mnt/shared/School/Textbooks/Robert\ W.\ Strayer\,\ Eric\ W.\ Nelson\ -\ Ways\ of\ the\ World_\ A\ Brief\ Global\ History\ with\ Sources\,\ Combined\ Volume-Bedford_St.\ Martin’s\ \(2015\).pdf &
-  #urxvt -e ranger /mnt/shared/School/woh-2012/Notes/ &
-  emacs /mnt/shared/School/woh-2012/Notes/ &
-elif [ "$selection" == "Watch TV" ]; then
+if [ "$selection" == "Watch TV" ]; then
   i3-msg "workspace $workspace; layout tabbed"
   chromium --new-window http://trakt.tv/dashboard &
   urxvt -e ranger /mnt/shared/saved-tv &
