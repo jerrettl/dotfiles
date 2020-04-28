6de056369aed0369a9079e2299c106541cfd4190
Jerrett Longworth <jerrett7@gmail.com>
2020-04-28 18:59:49 -0400

    select-monitor.sh: Remove old configurations
    Removing the machines:
      - arch-aspire-d255
      - inspiron-15-3537
      - dell-xps400
    
    Due to these machines mostly just no longer existing.

--- scripts/select-monitor.sh
+++ scripts/select-monitor.sh
@@ -3,37 +3,7 @@
 action="$1"
 hostname="$(hostname)"
 
-if [ "$hostname" == "arch-aspire-d255" ]; then
-	if [ "$action" == "primary" ]; then
-		xrandr --output LVDS1 --auto --output VGA1 --off
-	elif [ "$action" == "secondary" ]; then
-		xrandr --output LVDS1 --off --output VGA1 --auto
-	elif [ "$action" == "mirror" ]; then
-		xrandr --output LVDS1 --rate 60 --mode 800x600 --fb 800x600 --panning 800x600 --output VGA1 --mode 800x600 --same-as LVDS1
-	fi
-
-elif [ "$hostname" == "inspiron-15-3537" ]; then
-	if [ "$action" == "primary" ]; then
-		xrandr --output eDP1 --auto --output HDMI1 --off
-	elif [ "$action" == "secondary" ]; then
-		xrandr --output eDP1 --off --output HDMI1 --auto
-	elif [ "$action" == "mirror" ]; then
-		xrandr --output eDP1 --rate 60 --mode 1280x720 --fb 1280x720 --panning 1280x720 --output HDMI1 --mode 1280x720 --same-as eDP1
-	fi
-
-elif [ "$hostname" == "dell-xps400" ]; then
-	if [ "$action" == "primary" ]; then
-		xrandr --output "VGA-0" --auto --output "DVI-0" --off
-	elif [ "$action" == "secondary" ]; then
-		xrandr --output "VGA-0" --off --output "DVI-0" --auto
-	elif [ "$action" == "mirror" ]; then
-		xrandr --output "VGA-0" --rate 60 --mode 1280x1024 --fb 1280x1024 --panning 1280x1024 --output "DVI-0" --mode 1280x1024 --same-as "VGA-0"
-	elif [ "$action" == "extend" ]; then
-		xrandr --output "VGA-0" --auto --output "DVI-0" --off
-		xrandr --output "VGA-0" --primary --mode 1280x1024 --pos 0x0 --rotate normal --output "DVI-0" --mode 1280x1024 --pos 1280x0 --rotate normal
-	fi
-
-elif [ "$hostname" == "lg-gram" ]; then
+if [ "$hostname" == "lg-gram" ]; then
 	if [ "$action" == "primary" ]; then
 		xrandr --output "eDP1" --auto --output "HDMI1" --off
 	elif [ "$action" == "secondary" ]; then
