7e0ce8ff5203029a9fd373b373c70434329ed05f
Jerrett Longworth <jerrett7@gmail.com>
2020-05-30 13:46:41 -0400

    layouts: Add School layout

--- scripts/load-layout.sh
+++ scripts/load-layout.sh
@@ -8,7 +8,10 @@ if [ "$?" == "1" ]; then exit; fi
 workspace=$(echo | dmenu -nb "#303030" -nf "#ffffff" -sb "#ffffff" -sf "#303030" -fn "HelveticaNeue-11" -p "What workspace would you like to place it on?")
 if [ "$?" == "1" ]; then exit; fi
 
-if [ "$selection" == "Watch TV" ]; then
+if [ "$selection" == "School" ]; then
+  i3-msg "workspace $workspace; layout tabbed"
+	chromium --new-window https://webcourses.ucf.edu https://keep.google.com/u/0 &
+elif [ "$selection" == "Watch TV" ]; then
   i3-msg "workspace $workspace; layout tabbed"
   chromium --new-window http://trakt.tv/dashboard &
   urxvt -e ranger /mnt/shared/saved-tv &
