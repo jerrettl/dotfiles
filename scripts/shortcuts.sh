5ae444f13aeffca4c84ff062ae15c32d76e198b3
Jerrett Longworth <jerrett7@gmail.com>
2020-07-09 14:38:19 -0400

    shortcuts.sh: Remove space escapes for ranger

--- scripts/shortcuts.sh
+++ scripts/shortcuts.sh
@@ -9,11 +9,11 @@ bash_config_bind() {
 }
 
 ranger_folder_bind() {
-  cat "$1" | awk '{print "map g"$1" cd "substr($0, index($0, $2))"\nmap t"$1" tab_new "substr($0, index($0, $2))"\nmap m"$1" shell mv %s "substr($0, index($0, $2))"\nmap Y"$1" shell cp -r %s "substr($0, index($0, $2))""}' >> "$ranger_shortcuts"
+  cat "$1" | sed 's/\\//g' | awk '{print "map g"$1" cd "substr($0, index($0, $2))"\nmap t"$1" tab_new "substr($0, index($0, $2))"\nmap m"$1" shell mv %s "substr($0, index($0, $2))"\nmap Y"$1" shell cp -r %s "substr($0, index($0, $2))""}' >> "$ranger_shortcuts"
 }
 
 ranger_config_bind() {
-  cat "$1" | awk '{print "map "$1" shell $EDITOR "substr($0, index($0, $2))""}' >> "$ranger_shortcuts"
+  cat "$1" | sed 's/\\//g' | awk '{print "map "$1" shell $EDITOR "substr($0, index($0, $2))""}' >> "$ranger_shortcuts"
 }
 
 qute_folder_bind() {
