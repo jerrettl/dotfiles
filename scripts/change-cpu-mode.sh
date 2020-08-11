7831ea2b3dbc51a9086f9fb0f1ef4cc1e45f92ea
Jerrett Longworth <jerrett7@gmail.com>
2020-08-11 15:57:43 -0400

    change-cpu-mode.sh: Restore window focus and remove password

--- scripts/change-cpu-mode.sh
+++ scripts/change-cpu-mode.sh
@@ -9,18 +9,19 @@ arg="$1"
 
 apply_to_conf () {
   if [ "$1" == "power" ]; then
-    echo 0 | sudo tee /sys/devices/platform/lg-laptop/fan_mode
+    echo 0 | \sudo tee /sys/devices/platform/lg-laptop/fan_mode
   else
-    echo 1 | sudo tee /sys/devices/platform/lg-laptop/fan_mode
+    echo 1 | \sudo tee /sys/devices/platform/lg-laptop/fan_mode
   fi
 
-  echo "$1" | awk '{print "w /sys/devices/system/cpu/cpufreq/policy?/energy_performance_preference - - - - " $1}' | sudo tee /etc/tmpfiles.d/energy_performance_preference.conf
+  echo "$1" | awk '{print "w /sys/devices/system/cpu/cpufreq/policy?/energy_performance_preference - - - - " $1}' | \sudo tee /etc/tmpfiles.d/energy_performance_preference.conf
 }
 
 if [ "$arg" == "list" ]; then
   cat /sys/devices/system/cpu/cpufreq/policy?/energy_performance_preference
   exit
 elif [ "$arg" == "" ]; then
+  id=$(i3-msg -t get_tree | jq '.. | .id?, .focused?' | grep -B1 true | head -1)
   selection=$(cat /sys/devices/system/cpu/cpufreq/policy?/energy_performance_available_preferences | tail -n 1 | sed "s/\ /\\n/g" | sed "/^$/d" | dmenu)
   if [ "$?" == "0" ]; then
     apply_to_conf $selection
@@ -31,5 +32,7 @@ else
   apply_to_conf $arg
 fi
 
+
+[ -z "$id" ] || i3-msg focus "[id=$id]"
 sudo systemd-tmpfiles --create
 cat /sys/devices/system/cpu/cpufreq/policy?/energy_performance_preference
