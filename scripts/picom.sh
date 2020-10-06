f4a67210c260b684636f4b55181224e12b72a3ac
Jerrett Longworth <jerrett7@gmail.com>
2020-10-06 14:28:19 -0400

    i3/picom: Move all the functionality to picom.sh

--- scripts/picom.sh
+++ scripts/picom.sh
@@ -2,4 +2,13 @@
 
 hostname=$(uname -n)
 
-picom &
+if [ "$1" == "start" ]; then
+	notify-send "Starting picom."
+elif [ "$1" == "kill" ]; then
+	notify-send "Killing picom."
+	pkill picom
+	exit
+fi
+
+picom -b --xrender-sync-fence &
+
