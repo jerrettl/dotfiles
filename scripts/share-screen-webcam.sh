13c4272b11d7484391326ef91f7050c09bee191a
Jerrett Longworth <jerrett7@gmail.com>
2020-10-15 16:21:01 -0400

    share-screen-webcam.txt: Change to script
    This is not guaranteed to work in all cases, but this makes
    it a little easier to quickly get going instead of trying to
    copy-paste commands. Again, this is not going to work in all
    cases! This has a bunch of hardcoded values in the ffmpeg
    command right now.

--- scripts/share-screen-webcam.sh
+++ scripts/share-screen-webcam.sh
@@ -1,2 +1,9 @@
-modprobe v4l2loopback exclusive_caps=1
+#!/bin/bash
+
+arg=$1
+
+if [ "$arg" != "run" ]; then
+  sudo modprobe v4l2loopback exclusive_caps=1
+fi
+
 ffmpeg -f x11grab -r 30 -s 1920x1080 -i :0.0+0,0 -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 /dev/video2
