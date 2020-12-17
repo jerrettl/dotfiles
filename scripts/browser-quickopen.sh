950abc33b39ed0617119df2f0fd27c7999dc611d
Jerrett Longworth <jerrett7@gmail.com>
2020-12-16 20:15:03 -0500

    Move websites.txt to notes/

--- scripts/browser-quickopen.sh
+++ scripts/browser-quickopen.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-site=$(cat ~/dotfiles/websites.txt | dmenu -p 'Enter a URL:' -i -l 10 -nb "#303030" -nf "#ffffff" -sb "#ffffff" -sf "#303030" -fn "HelveticaNeue-11")
+site=$(cat $DOTFILES/notes/websites.txt | dmenu -p 'Enter a URL:' -i -l 10 -nb "#303030" -nf "#ffffff" -sb "#ffffff" -sf "#303030" -fn "HelveticaNeue-11")
 if [ "$?" == "0" ]; then firefox --new-window $(echo $site | sed "s/\[.*\] //"); fi
