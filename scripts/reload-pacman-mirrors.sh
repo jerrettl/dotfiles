728e66c7dad7d3db37be0f29b0bde6a01b9b5a5e
Jerrett Longworth <jerrett7@gmail.com>
2021-01-29 00:27:57 -0500

    reload-pacman-mirrors.sh: Show mirrors being ranked

--- scripts/reload-pacman-mirrors.sh
+++ scripts/reload-pacman-mirrors.sh
@@ -8,6 +8,7 @@ else
 	mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
 	wget "https://www.archlinux.org/mirrorlist/?country=US&protocol=https&ip_version=4" -O /etc/pacman.d/mirrorlist.new
 	sed --in-place -E 's/^#(.*)$/\1/' /etc/pacman.d/mirrorlist.new
+	echo "Ranking mirrors..."
 	rankmirrors -n 10 /etc/pacman.d/mirrorlist.new > /etc/pacman.d/mirrorlist
 	rm /etc/pacman.d/mirrorlist.new
 fi
