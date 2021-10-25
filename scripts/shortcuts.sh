54e289f782d252634cbd287c2a5c334c22e17aec
jerrettl <jerrett7@gmail.com>
2021-10-25 10:02:03 -0400

    shortcuts.sh: Fix indentation

--- scripts/shortcuts.sh
+++ scripts/shortcuts.sh
@@ -9,8 +9,8 @@ bash_config_bind() {
 }
 
 ranger_folder_bind() {
-	cat "$1" | sed 's/\\//g' | sed -E 's/\$([A-Z_]*)/\"\ \+\ os.getenv('"'"'\1\'"'"')\ +\ \"/' | awk '{print "map g"$1" eval fm.cd(\""substr($0, index($0, $2))"\")"}' >> "$ranger_shortcuts"
-	cat "$1" | sed 's/\\//g' | sed -E 's/\$([A-Z_]*)/\"\ \+\ os.getenv('"'"'\1\'"'"')\ +\ \"/' | awk '{print "map t"$1" eval fm.tab_new(\""substr($0, index($0, $2))"\")"}' >> "$ranger_shortcuts"
+  cat "$1" | sed 's/\\//g' | sed -E 's/\$([A-Z_]*)/\"\ \+\ os.getenv('"'"'\1\'"'"')\ +\ \"/' | awk '{print "map g"$1" eval fm.cd(\""substr($0, index($0, $2))"\")"}' >> "$ranger_shortcuts"
+  cat "$1" | sed 's/\\//g' | sed -E 's/\$([A-Z_]*)/\"\ \+\ os.getenv('"'"'\1\'"'"')\ +\ \"/' | awk '{print "map t"$1" eval fm.tab_new(\""substr($0, index($0, $2))"\")"}' >> "$ranger_shortcuts"
   cat "$1" | sed 's/\\//g' | awk '{print "map m"$1" shell mv %s "substr($0, index($0, $2))""}' >> "$ranger_shortcuts"
   cat "$1" | sed 's/\\//g' | awk '{print "map Y"$1" shell cp -r %s "substr($0, index($0, $2))""}' >> "$ranger_shortcuts"
 }
