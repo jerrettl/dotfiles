4eb57f210b32beaa847a21dcbacedf13b60e777a
Jerrett Longworth <jerrett7@gmail.com>
2022-05-24 20:22:02 -0400

    reload-pacman-mirrors.sh: Update mirrorlist URL

--- scripts/reload-pacman-mirrors.sh
+++ scripts/reload-pacman-mirrors.sh
@@ -6,7 +6,7 @@ if [ "$(id -u)" -ne 0 ]; then
 	exit
 else
 	mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
-	wget "https://www.archlinux.org/mirrorlist/?country=US&protocol=https&ip_version=4" -O /etc/pacman.d/mirrorlist.new
+	wget "https://archlinux.org/mirrorlist/?country=US&protocol=https&ip_version=4&use_mirror_status=on" -O /etc/pacman.d/mirrorlist.new
 	sed --in-place -E 's/^#(.*)$/\1/' /etc/pacman.d/mirrorlist.new
 	echo "Ranking mirrors..."
 	rankmirrors -n 10 /etc/pacman.d/mirrorlist.new > /etc/pacman.d/mirrorlist
