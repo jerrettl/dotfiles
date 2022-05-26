8852752b1a8617759873623e9624b26832ae766f
Jerrett Longworth <jerrett7@gmail.com>
2022-05-26 16:34:28 -0400

    offlineimap: Update for Python 3
    offlineimap uses Python 3 in version >= 8.0.0

--- .config/mail/offlineimap/offlineimap.py
+++ .config/mail/offlineimap/offlineimap.py
@@ -1,5 +1,5 @@
-#! /usr/bin/env python2
+#! /usr/bin/env python3
 from subprocess import check_output
 
 def get_pass(account):
-    return check_output("gpg -dq ~/.config/neomutt/accounts/" + account + "/" + account + ".gpg", shell=True).strip("\n")
+    return check_output("gpg -dq ~/.config/neomutt/accounts/" + account + "/" + account + ".gpg", shell=True).strip()
