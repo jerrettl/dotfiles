79c72c81acf9ab5e4e3edb52fa45f178ec5a5a0e
Jerrett Longworth <jerrett7@gmail.com>
2024-02-14 14:08:54 -0500

    reload-pacman-mirrors.sh: Add date to mirrorlist backup

--- scripts/reload-pacman-mirrors.sh
+++ scripts/reload-pacman-mirrors.sh
@@ -5,7 +5,7 @@ if [ "$(id -u)" -ne 0 ]; then
 	echo "Please run this script as root."
 	exit
 else
-	mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
+	mv /etc/pacman.d/mirrorlist "/etc/pacman.d/mirrorlist.$(date --iso-8601=seconds).backup"
 	wget "https://archlinux.org/mirrorlist/?country=US&protocol=https&ip_version=4&use_mirror_status=on" -O /etc/pacman.d/mirrorlist.new
 	sed --in-place -E 's/^#(.*)$/\1/' /etc/pacman.d/mirrorlist.new
 	echo "Ranking mirrors..."
