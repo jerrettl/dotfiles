a8cb2ce76deac893cc5beaae8d15bce06786180b
Jerrett Longworth <jerrett7@gmail.com>
2024-02-26 16:01:24 -0500

    per-machine-variables.sh: Add thinkpad trackpad settings

--- scripts/per-machine-variables.sh
+++ scripts/per-machine-variables.sh
@@ -36,6 +36,12 @@ elif [ "$hostname" == "thinkpad" ]; then
 	# Display scaling
 	export GDK_SCALE=2
 
+	# Touchpad acceleration adjustment
+	id=$(xinput list | grep -i "Touchpad" | grep -oP 'id=[0-9]*' | sed 's/id=//')
+	property=$(xinput list-props "$id" | grep -i 'libinput Accel Speed (' | cut -d "(" -f2 | cut -d ")" -f1)
+	accel="0.33"
+	xinput set-prop "$id" "$property" "$accel"
+
 	# Fix for audio devices not being fully loaded at first start
 	sleep 2
 
